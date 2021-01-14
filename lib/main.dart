9e5d25b3d7545e3f63117f05c83bcaa20660b77f
Sangbum Woo <Sangbum.Woo@partner.bmw.co.kr>
2021-01-15 08:54:53 +0900

    initial-commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:carrot_market_sample/page/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.white, // 테마가 흰색으로 설정됨.
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
