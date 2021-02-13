6ae91f7c55975d57a4cea91532e863e6c1b35540
Sangbum Woo <sbwoo87@gmail.com>
2021-02-14 08:02:20 +0900

    add detail page

---
+++ lib/pages/detail.dart
@@ -0,0 +1,368 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:carrot_market_sample/components/manor_templature_widget.dart';
+import 'package:carrot_market_sample/utils/data_utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class DetailContentView extends StatefulWidget {
+  Map<String, String> data;
+  DetailContentView({Key key, this.data}) : super(key: key);
+
+  @override
+  _DetailContentViewState createState() => _DetailContentViewState();
+}
+
+class _DetailContentViewState extends State<DetailContentView>
+    with SingleTickerProviderStateMixin {
+  Size size;
+  List<Map<String, String>> imgList;
+  int _current;
+  double scrollpositionToAlpha = 0;
+  ScrollController _controller = ScrollController();
+  AnimationController _animationController;
+  Animation _colorTween;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(vsync: this);
+    _colorTween = ColorTween(begin: Colors.white, end: Colors.black)
+        .animate(_animationController);
+    _controller.addListener(() {
+      setState(() {
+        if (_controller.offset > 255) {
+          scrollpositionToAlpha = 255;
+        } else {
+          scrollpositionToAlpha = _controller.offset;
+        }
+        _animationController.value = scrollpositionToAlpha / 255;
+        // print("${_controller.offset}, ${_animationController.value}");
+      });
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    size = MediaQuery.of(context).size;
+    imgList = [
+      {"id": "0", "url": widget.data["image"]},
+      {"id": "1", "url": widget.data["image"]},
+      {"id": "2", "url": widget.data["image"]},
+      {"id": "3", "url": widget.data["image"]},
+      {"id": "4", "url": widget.data["image"]},
+    ];
+    _current = 0;
+  }
+
+  Widget _makeIcon(IconData icon) {
+    return AnimatedBuilder(
+      animation: _colorTween,
+      builder: (context, child) => Icon(icon, color: _colorTween.value),
+    );
+  }
+
+  Widget _appbarWidget() {
+    return AppBar(
+      backgroundColor: Colors.white.withAlpha(scrollpositionToAlpha.toInt()),
+      leading: IconButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: _makeIcon(Icons.arrow_back),
+      ),
+      elevation: 0,
+      actions: [
+        IconButton(icon: _makeIcon(Icons.share), onPressed: () {}),
+        IconButton(icon: _makeIcon(Icons.more_vert), onPressed: () {}),
+      ],
+    );
+  }
+
+  Widget _makeSliderImage() {
+    return Container(
+      child: Stack(
+        children: [
+          Hero(
+            tag: widget.data["cid"],
+            child: CarouselSlider(
+              options: CarouselOptions(
+                  height: size.width,
+                  initialPage: 0,
+                  enableInfiniteScroll: false,
+                  viewportFraction: 1,
+                  autoPlay: false,
+                  enlargeCenterPage: false,
+                  aspectRatio: 2.0,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _current = index;
+                    });
+                  }),
+              items: imgList.map((map) {
+                return Image.asset(map["url"],
+                    width: size.width, fit: BoxFit.fill);
+              }).toList(),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: imgList.map((map) {
+                // int index = imgList.indexOf(url);
+                return Container(
+                  width: 10.0,
+                  height: 10.0,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _current == int.parse(map["id"])
+                        ? Colors.white
+                        : Colors.white.withOpacity(0.4),
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _sellerSimpleInfo() {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 25,
+            backgroundImage: Image.asset("assets/images/user.png").image,
+          ),
+          SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "개발하는 남자",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              Text("제주시 도담동"),
+            ],
+          ),
+          Expanded(
+            child: ManorTemplature(
+              manorTemp: 37.5,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _line() {
+    return Container(
+      height: 1,
+      color: Colors.grey.withOpacity(0.3),
+      margin: const EdgeInsets.symmetric(horizontal: 15),
+    );
+  }
+
+  Widget _contentDetail() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(height: 20),
+          Text(
+            widget.data["title"],
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          Text(
+            "디지털/가전 . 22시간 전",
+            style: TextStyle(
+              color: Colors.grey,
+              fontWeight: FontWeight.bold,
+              fontSize: 10,
+            ),
+          ),
+          SizedBox(height: 15),
+          Text(
+            "선물받은 새상품이고,\n보호필름 붙여서 사용하여 스크래치 없습니다.\n직거래만 진행합니다.",
+            style: TextStyle(fontSize: 15, height: 1.5),
+          ),
+          SizedBox(height: 15),
+          Text(
+            "채팅 3 . 관심 17 . 조회 295",
+            style: TextStyle(
+              color: Colors.grey,
+              fontWeight: FontWeight.bold,
+              fontSize: 12,
+            ),
+          ),
+          SizedBox(height: 15),
+        ],
+      ),
+    );
+  }
+
+  Widget _otherSellContents() {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "판매자님의 판매 상품",
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "모두보기",
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _bodyWidget() {
+    return CustomScrollView(
+      controller: _controller,
+      slivers: [
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
+              _makeSliderImage(),
+              _sellerSimpleInfo(),
+              _line(),
+              _contentDetail(),
+              _line(),
+              _otherSellContents(),
+            ],
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          sliver: SliverGrid(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10),
+            delegate: SliverChildListDelegate(
+              List.generate(20, (index) {
+                return Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                          color: Colors.grey,
+                          height: 120,
+                        ),
+                      ),
+                      Text(
+                        "셈플 제목",
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      Text("금액",
+                          style: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.bold))
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _bottombarWidget() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      width: size.width,
+      height: 55,
+      child: Row(
+        children: [
+          GestureDetector(
+            onTap: () {
+              print("관심상품 이벤트 발생");
+            },
+            child: SvgPicture.asset(
+              "assets/svg/heart_off.svg",
+              width: 25,
+              height: 25,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 15, right: 10),
+            width: 1,
+            height: 40,
+            color: Colors.black.withOpacity(0.3),
+          ),
+          Column(
+            children: [
+              Text(
+                DataUtils.calcStringToWon(widget.data["price"]),
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text("가격제안불가",
+                  style: TextStyle(fontSize: 14, color: Colors.grey)),
+            ],
+          ),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: Color(0xfff08f4f),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
+                  child: Text(
+                    "채팅으로 거래하기",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: _appbarWidget(),
+      body: _bodyWidget(),
+      bottomNavigationBar: _bottombarWidget(),
+    );
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,9 @@
+import 'package:carrot_market_sample/pages/detail.dart';
+import 'package:carrot_market_sample/repository/contents_repository.dart';
+import 'package:carrot_market_sample/utils/data_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:intl/intl.dart';
+// import 'package:intl/intl.dart';
 
 class Home extends StatefulWidget {
   Home({Key key}) : super(key: key);
@@ -17,93 +20,13 @@ class _HomeState extends State<Home> {
     "donam": "도남동",
   };
   String currentLocation;
+  ContentsRepository contentsRepository;
 
   @override
   void initState() {
     super.initState();
     currentLocation = "ara";
-    datas = [
-      {
-        "cid": "1",
-        "image": "assets/images/ara-1.jpg",
-        "title": "네메시스 축구화275",
-        "location": "제주 제주시 아라동",
-        "price": "30000",
-        "likes": "2"
-      },
-      {
-        "cid": "2",
-        "image": "assets/images/ara-2.jpg",
-        "title": "LA갈비 5kg팔아요~",
-        "location": "제주 제주시 아라동",
-        "price": "100000",
-        "likes": "5"
-      },
-      {
-        "cid": "3",
-        "image": "assets/images/ara-3.jpg",
-        "title": "치약팝니다",
-        "location": "제주 제주시 아라동",
-        "price": "5000",
-        "likes": "0"
-      },
-      {
-        "cid": "4",
-        "image": "assets/images/ara-4.jpg",
-        "title": "[풀박스]맥북프로16인치 터치바 스페이스그레이",
-        "location": "제주 제주시 아라동",
-        "price": "2500000",
-        "likes": "6"
-      },
-      {
-        "cid": "5",
-        "image": "assets/images/ara-5.jpg",
-        "title": "디월트존기임팩",
-        "location": "제주 제주시 아라동",
-        "price": "150000",
-        "likes": "2"
-      },
-      {
-        "cid": "6",
-        "image": "assets/images/ara-6.jpg",
-        "title": "갤럭시s10",
-        "location": "제주 제주시 아라동",
-        "price": "180000",
-        "likes": "2"
-      },
-      {
-        "cid": "7",
-        "image": "assets/images/ara-7.jpg",
-        "title": "선반",
-        "location": "제주 제주시 아라동",
-        "price": "15000",
-        "likes": "2"
-      },
-      {
-        "cid": "8",
-        "image": "assets/images/ara-8.jpg",
-        "title": "냉장 쇼케이스",
-        "location": "제주 제주시 아라동",
-        "price": "80000",
-        "likes": "3"
-      },
-      {
-        "cid": "9",
-        "image": "assets/images/ara-9.jpg",
-        "title": "대우 미니냉장고",
-        "location": "제주 제주시 아라동",
-        "price": "30000",
-        "likes": "3"
-      },
-      {
-        "cid": "10",
-        "image": "assets/images/ara-10.jpg",
-        "title": "멜킨스 풀업 턱걸이 판매합니다.",
-        "location": "제주 제주시 아라동",
-        "price": "50000",
-        "likes": "7"
-      },
-    ];
+    datas = [];
 
     print("initState - currentLocation : $currentLocation");
   }
@@ -112,6 +35,7 @@ class _HomeState extends State<Home> {
   void didChangeDependencies() {
     super.didChangeDependencies();
     print("didChangeDependencies - currentLocation : $currentLocation");
+    contentsRepository = ContentsRepository();
   }
 
   Widget _appBarWidget() {
@@ -158,66 +82,100 @@ class _HomeState extends State<Home> {
     );
   }
 
-  final oCcy = new NumberFormat('#,###', "ko_kr");
-  String getPriceFromString(String price) {
-    return "${oCcy.format(int.parse(price))}원";
+  loadContents() {
+    return contentsRepository.loadContnetsFromLocation(currentLocation);
   }
 
-  Widget _bodyWidget() {
+  Widget makeDataList(datas) {
     return ListView.separated(
         padding: EdgeInsets.symmetric(horizontal: 10),
         itemBuilder: (BuildContext _context, int index) {
-          return Container(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              child: Row(
-                children: [
-                  ClipRRect(
-                      child: Image.asset(datas[index]["image"],
-                          width: 100, height: 100),
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.only(left: 10),
-                      height: 100,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            datas[index]["title"],
-                            style: TextStyle(fontSize: 15),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          SizedBox(height: 5),
-                          Text(datas[index]["location"],
-                              style: TextStyle(
-                                  color: Colors.black.withOpacity(0.3))),
-                          SizedBox(height: 5),
-                          Text(getPriceFromString(datas[index]["price"]),
-                              style: TextStyle(fontWeight: FontWeight.w500)),
-                          SizedBox(height: 5),
-                          Expanded(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                SvgPicture.asset('assets/svg/heart_off.svg',
-                                    width: 13, height: 13),
-                                SizedBox(width: 5),
-                                Text(datas[index]["likes"]),
-                              ],
+          return GestureDetector(
+            onTap: () {
+              print(datas[index]["title"]);
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) =>
+                          DetailContentView(data: datas[index])));
+            },
+            child: Container(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  children: [
+                    ClipRRect(
+                        child: Hero(
+                          tag: datas[index]["cid"],
+                          child: Image.asset(datas[index]["image"],
+                              width: 100, height: 100),
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.only(left: 10),
+                        height: 100,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              datas[index]["title"],
+                              style: TextStyle(fontSize: 15),
+                              overflow: TextOverflow.ellipsis,
                             ),
-                          ),
-                        ],
+                            SizedBox(height: 5),
+                            Text(datas[index]["location"],
+                                style: TextStyle(
+                                    color: Colors.black.withOpacity(0.3))),
+                            SizedBox(height: 5),
+                            Text(
+                                DataUtils.calcStringToWon(
+                                    datas[index]["price"]),
+                                style: TextStyle(fontWeight: FontWeight.w500)),
+                            SizedBox(height: 5),
+                            Expanded(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  SvgPicture.asset('assets/svg/heart_off.svg',
+                                      width: 13, height: 13),
+                                  SizedBox(width: 5),
+                                  Text(datas[index]["likes"]),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  )
-                ],
-              ));
+                    )
+                  ],
+                )),
+          );
         },
         separatorBuilder: (BuildContext _context, int index) {
           return Container(height: 1, color: Colors.black.withOpacity(0.4));
         },
         itemCount: 10);
+  }
+
+  Widget _bodyWidget() {
+    return FutureBuilder(
+        future: loadContents(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text("데이터 오류"));
+          }
+
+          if (snapshot.hasData) {
+            return makeDataList(snapshot.data);
+          }
+
+          return Center(child: Text("해당지역에 데이터가 없습니다."));
+        });
   }
 
   @override
